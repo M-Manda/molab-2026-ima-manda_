86f161c076923d9a8595bdb856a6e3d87ccfd417
M-Manda <uurdmandakhmts@gmail.com>
2026-09-11 09:17:14 -0400

    h

---
+++ week1/MyPlayground1.playground/Pages/image.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+
+for i in 1...5 {
+    for j in 1...i {
+        print("🌟", terminator: "")
+    }
+    print()
+}
+var a = 0
+for i in 1...5 {
+    a = 5 - i
+    for j in 1...a {
+        print("🌟", terminator: "")
+    }
+    print()
+}
