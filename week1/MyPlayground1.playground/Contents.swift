e05a6e437e96b87834cf3c39e124ad1adc1e9d65
M-Manda <uurdmandakhmts@gmail.com>
2026-09-10 20:37:45 -0400

    h

--- week1/MyPlayground1.playground/Contents.swift
+++ week1/MyPlayground1.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+print("Hello, world!")
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let implicitInteger = 70
+let ImplicitDouble = 70.0
+let explicitDouble: Double = 70
+
+
